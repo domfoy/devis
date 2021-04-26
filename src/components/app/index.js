1ba402ecd1c3fd07cf09eaccadc1d40bcb80c3d8
quentin.pergeline <quentin.pergeline@iwecloud.com>
2021-04-26 20:44:05 +0200

    feat: removed test form component

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -5,14 +5,6 @@ function App(props) {
   return (
     <article className="App">
       <Form data={props.data}/>
-      <form
-        action="/wp-admin/admin-ajax.php?action=devis_send_quote_mail"
-        method="POST"
-      >
-        <label for="fname">First name:</label>
-        <input type="text" id="fname" name="fname"></input>
-        <input type="submit" value="Submit"></input>
-      </form>
     </article>
   );
 }
