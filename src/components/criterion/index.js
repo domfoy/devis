04474db5ee12c33b3cbe2f9365adfe8f15483aae
quentin.pergeline <quentin.pergeline@iwecloud.com>
2021-05-01 17:28:17 +0200

    feat(criterion): better epaisseur label

--- src/components/criterion/index.js
+++ src/components/criterion/index.js
@@ -24,20 +24,27 @@ function buildItems(items) {
   );
 }
 
-function mkRenderValue(items) {
+function mkRenderValue(criterionLabel, items) {
   return (value) => {
     const item = _.find(
       items,
       {value}
     );
-    return item.value;
+
+    return criterionLabel === 'Épaisseur'
+      ? `${item.value} mm`
+      : item.value;
   }
 }
 
 function Criterion(props) {
   const classes = useStyles();
+
+  if (props.itemValues.length === 1 && props.itemValues[0].key === '') {
+    return null;
+  }
   const items = buildItems(props.itemValues);
-  const renderValue = mkRenderValue(props.itemValues);
+  const renderValue = mkRenderValue(props.label, props.itemValues);
 
   return <FormControl className={classes.formControl}>
     <InputLabel id="demo-controlled-open-select-label">{props.label}</InputLabel>
